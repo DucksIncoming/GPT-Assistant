d701103046aa1e378e278c60902a4b160e95121e
DucksIncoming <50174663+DucksIncoming@users.noreply.github.com>
2023-09-14 13:36:03 -0400

    Init

---
+++ gpt-assistant.js
@@ -0,0 +1,20 @@
+/*
+
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: 'my api key',
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(completion.choices);
+}
+
+main();
+
+*/
